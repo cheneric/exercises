d669934fcfd086acb309b48bdc08de724419e912
Eric Chen <cheneric@gmail.com>
2017-07-29 10:11:46 -0700

    explained CoinChange logic in comments

--- src/main/java/cheneric/exercise/CoinChange.java
+++ src/main/java/cheneric/exercise/CoinChange.java
@@ -6,37 +6,63 @@ import java.util.List;
 /**
  * <p>Calculates change using coins.</p>
  *
- * <p>To run:</p>
+ * <p>To run from the project root:</p>
  * <code>
  *   ./gradlew run -q -PmainClass=cheneric.exercise.CoinChange
  * </code>
  */
 public class CoinChange {
+	/**
+	 * Returns a collection of the smallest number of coins that totals the requested <code>amount</code>.
+	 * 
+	 * @param amount the desired value of the collection of coins.
+	 * @param coinValues the available coin denominations.
+	 * @return a collection of the smallest number of coins that totals the requested <code>amount</code>, 
+	 * or <code>null</code> if no solution exists.
+	 */
 	public static List<Integer> minCoins(int amount, int... coinValues) {
 		if (amount >= 0) {
+			// cache of smallest number of coins that total each subAmount from 0 to the desired amount
 			final List<List<Integer>> minCoins = new ArrayList<List<Integer>>(amount + 1);
+			// no coins required to total 0
 			minCoins.add(new ArrayList<>());
-			// initialize
+			// initially, no known way to get any of the other subAmounts
 			for (int count = 0; count < amount; count++) {
 				minCoins.add(null);
 			}
-			for (int amountCount = 0; amountCount <= amount; amountCount++) {
+
+			// increment through each subAmount
+			for (int subAmount = 0; subAmount <= amount; subAmount++) {
+				// for each coinValue
 				for (final int coinValue : coinValues) {
-					if (coinValue <= amountCount) {
-						final int subValue = amountCount - coinValue;
-						final List<Integer> subMinCoins = minCoins.get(subValue);
-						if (subMinCoins != null) {
-							final List<Integer> currentMinCoins = minCoins.get(amountCount);
-							if (currentMinCoins != null && subMinCoins.size() + 1 >= currentMinCoins.size()) {
+					if (coinValue <= subAmount) {
+						final int dependentAmount = subAmount - coinValue;
+						final List<Integer> dependentMinCoins = minCoins.get(dependentAmount);
+
+						// there is a coin combination totalling this subAmount iff we have 
+						// previously found a dependentAmount totalling (subAmount - coinValue)
+						if (dependentMinCoins != null) {
+							final List<Integer> currentMinCoins = minCoins.get(subAmount);
+							// if a previous combination of coins was found totaling this 
+							// subAmount but the number of coins in dependentMinCoins plus 
+							// this current coin would be greater than the number of coins 
+							// in the previously found combination of coins, keep the 
+							// previous combination of coins
+							if (currentMinCoins != null && dependentMinCoins.size() + 1 >= currentMinCoins.size()) {
 								continue;
 							}
-							final List<Integer> newMinCoins = new ArrayList<>(subMinCoins);
+							// otherwise, replace the previous combination of coins with a 
+							// smaller combination consisting of a copy of the collection 
+							// totalling (subAmount - coinValue) plus the current coinValue
+							final List<Integer> newMinCoins = new ArrayList<>(dependentMinCoins);
 							newMinCoins.add(coinValue);
-							minCoins.set(amountCount, newMinCoins);
+							minCoins.set(subAmount, newMinCoins);
 						}
 					}
 				}
 			}
+
+			// return the solution
 			return minCoins.get(amount);
 		}
 		else {
